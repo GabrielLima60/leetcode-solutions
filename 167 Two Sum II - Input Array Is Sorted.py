70a5457be4858535075beefdcc2536ae3d46bf7e
Gabriel Lima <73233318+GabrielLima60@users.noreply.github.com>
2023-08-05 10:32:50 -0300

    Create 167 Two Sum II - Input Array Is Sorted.py

---
+++ 167 Two Sum II - Input Array Is Sorted.py
@@ -0,0 +1,23 @@
+#Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 < numbers.length.
+
+#Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+
+#The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+#Your solution must use only constant extra space.
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        first = 0
+        last = -1 
+
+        while True:
+            num_sum = numbers[first] + numbers[last]
+            if num_sum > target:
+                last -=  1
+            
+            elif num_sum < target:
+                first += 1
+            
+            else:
+                return [first+1, len(numbers) + last + 1]
