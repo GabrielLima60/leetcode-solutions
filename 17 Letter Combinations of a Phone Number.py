531f0ba92bfa4ce7fae19de33d330e33b3220afd
Gabriel Lima <73233318+GabrielLima60@users.noreply.github.com>
2023-12-30 16:38:14 -0300

    Create 17 Letter Combinations of a Phone Number.py

---
+++ 17 Letter Combinations of a Phone Number.py
@@ -0,0 +1,29 @@
+'''Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+
+A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.'''
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+
+        if digits == '':
+            return []
+
+        dict_digits = {"2": ['a', 'b', 'c'],
+                        "3": ['d', 'e', 'f'],
+                        "4": ['g', 'h', 'i'],
+                        "5": ['j', 'k', 'l'],
+                        "6": ['m', 'n', 'o'],
+                        "7": ['p', 'q', 'r', 's'],
+                        "8": ['t', 'u', 'v'],
+                        "9": ['w', 'x', 'y', 'z']
+        }
+
+        output = [[]]
+
+        for digit in digits:
+            output = [[a, b] for a in output for b in dict_digits[digit]]
+            output = [inner[0] + [inner[1]] for inner in output]
+
+        output = [''.join(inner) for inner in output]
+
+        return output
